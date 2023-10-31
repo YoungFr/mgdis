1f62c39d6e487fe45877bac7c9fec909dbcedec1
Yinlei Meng <mengyl4772@gmail.com>
2023-10-31 21:32:07 +0800

    add SET and GET command

--- handlers.go
+++ handlers.go
@@ -1,7 +1,11 @@
 package main
 
+import "sync"
+
 var handlers = map[string]func([]Data) Data{
 	"PING": ping,
+	"SET":  set,
+	"GET":  get,
 }
 
 func ping(args []Data) Data {
@@ -21,6 +25,63 @@ func ping(args []Data) Data {
 		{
 			reply.dataType = datatypes[ERROR]
 			reply.errorMsg = "ERR wrong number of arguments for 'ping' command"
+		}
+	}
+	return reply
+}
+
+type Pairs struct {
+	kv map[string]string
+	mu sync.RWMutex
+}
+
+var p = Pairs{
+	kv: make(map[string]string),
+	mu: sync.RWMutex{},
+}
+
+func set(args []Data) Data {
+	var reply Data
+	switch len(args) {
+	case 2:
+		{
+			k, v := args[0].bulkStr, args[1].bulkStr
+			p.mu.Lock()
+			p.kv[k] = v
+			p.mu.Unlock()
+			reply.dataType = datatypes[SIMPLE_STRING]
+			reply.simpleStr = "OK"
+		}
+	default:
+		{
+			reply.dataType = datatypes[ERROR]
+			reply.errorMsg = "ERR wrong number of arguments for 'set' command"
+		}
+	}
+	return reply
+}
+
+func get(args []Data) Data {
+	var reply Data
+	switch len(args) {
+	case 1:
+		{
+			k := args[0].bulkStr
+			p.mu.Lock()
+			v, ok := p.kv[k]
+			p.mu.Unlock()
+			if ok {
+				reply.dataType = datatypes[BULK_STRING]
+				reply.bulkStr = v
+			} else {
+				reply.dataType = datatypes[BULK_STRING]
+				reply.isNullBulk = true
+			}
+		}
+	default:
+		{
+			reply.dataType = datatypes[ERROR]
+			reply.errorMsg = "ERR wrong number of arguments for 'get' command"
 		}
 	}
 	return reply
